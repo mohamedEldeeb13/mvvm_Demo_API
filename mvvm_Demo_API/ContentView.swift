41564ea9481c4686cc21bfcf2b1c178707f96bae
mohamedEldeeb13 <mooel744@gmail.com>
2023-12-25 16:01:44 +0200

    Initial Commit

---
+++ mvvm_Demo_API/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mvvm_Demo_API
+//
+//  Created by Mohamed Abd Elhakam on 25/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mvvm_Demo_API/mvvm_Demo_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  mvvm_Demo_APIApp.swift
+//  mvvm_Demo_API
+//
+//  Created by Mohamed Abd Elhakam on 25/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct mvvm_Demo_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
